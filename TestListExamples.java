5c6fb53b599725136611dbe285b0b5691fe1ad02
Yong Gyun Kim <edward@edykim.com>
2022-11-14 21:40:39 -0800

    Add grade script with test cases

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,71 @@
 import static org.junit.Assert.*;
+import java.util.List;
+import java.util.Arrays;
 import org.junit.*;
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
+  @Test(timeout=100)
+  public void shouldCreate() {
+    ListExamples example = new ListExamples();
   }
-  // Write your grading tests here!
+  @Test(timeout=100)
+  public void shouldFilter() {
+    List<String> expected = Arrays.asList(
+        new String[] {
+          "apple",
+          "apple computer",
+        });
+    List<String> haystack = Arrays.asList(
+        new String[] {
+          "apple",
+          "banana",
+          "banana computer",
+          "apple computer",
+        });
+
+    List<String> filtered = ListExamples.filter(haystack, (String s) -> s.contains("apple"));
+
+    assertEquals(expected, filtered);
+  }
+  @Test(timeout=100)
+  public void shouldMerge() {
+    List<String> expected = Arrays.asList(
+        new String[] {
+          "apple",
+          "apple computer",
+          "banana",
+          "banana computer",
+        });
+    List<String> fruits = Arrays.asList(
+        new String[] {
+          "apple",
+          "banana",
+        });
+    List<String> companies = Arrays.asList(
+        new String[] {
+          "apple computer",
+          "banana computer",
+        });
+
+    List<String> actual = ListExamples.merge(fruits, companies);
+    assertEquals(expected, actual);
+  }
+  @Test(timeout=100)
+  public void shouldMergeWithSameValue() {
+    List<String> expected = Arrays.asList(
+        new String[] {
+          "apple",
+          "apple",
+          "banana",
+          "banana",
+        });
+    List<String> fruits = Arrays.asList(
+        new String[] {
+          "apple",
+          "banana",
+        });
+
+    List<String> actual = ListExamples.merge(fruits, fruits);
+    assertEquals(expected, actual);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,72 @@
 # Create your grading script here
 
+# set -e
+
+EXAMPLE_FILE_NAME="ListExamples.java"
+CLASS_NAME="class ListExamples"
+
+echo "Code Grader"
+echo "Target URL: $1"
+echo ""
+
 rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone $1 student-submission -q
+
+if [[ $? -eq 0 ]]
+then
+  echo "[PASSED] Cloned successfully."
+else
+  echo "[FAILED] Clone failed. Check the submit URL."
+  exit 1
+fi
+
+cd student-submission
+
+if [[ -f $EXAMPLE_FILE_NAME ]]
+then
+  echo "[PASSED] File Found ($EXAMPLE_FILE_NAME)"
+else
+  echo "[FAILED] File Not Found ($EXAMPLE_FILE_NAME)"
+  exit 1
+fi
+
+grep -q "$CLASS_NAME" ./$EXAMPLE_FILE_NAME
+
+if [[ $? -eq 0 ]]
+then
+  echo "[PASSED] Text Found ($CLASS_NAME)"
+else
+  echo "[FAILED] Text Not Found ($CLASS_NAME)"
+  exit 1
+fi
+
+CP=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+
+cp ../TestListExamples.java .
+javac -cp $CP *.java 2> compile_error.txt
+
+if [[ $? -eq 0 ]]
+then
+  echo "[PASSED] Compiled successfully."
+else
+  MSG=`cat compile_error.txt | head -1`
+  echo "[FAILED] Compile Failed. ($MSG)"
+  exit 1
+fi
+
+rm -rf test_result.txt
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > test_result.txt
+
+JUNIT_EXIT_CODE=$?
 
 
-
+if [[ $JUNIT_EXIT_CODE -eq 0 ]]
+then
+  RESULT=`cat test_result.txt | grep "OK"`
+  echo "[PASSED] jUnit result: " $RESULT
+else
+  RESULT=`cat test_result.txt | grep "Tests run:"`
+  echo "[FAILED] jUnit result: " $RESULT
+  exit 1
+fi
 
